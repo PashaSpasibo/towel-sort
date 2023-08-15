f851b1f88758d09f8bb256f07098b61041bb27bc
rb.nmd <pav.tro98@icloud.com>
2023-08-15 16:03:39 +0400

    task done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 
-// You should implement your task here.
+// // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if (matrix){
+    let res = []
+    for (i in matrix){
+      i % 2 === 0 ? res.push(matrix[i]) :
+                    res.push(matrix[i].reverse())
+    }
+    return res.flat()
+  }
+  return []
 }
